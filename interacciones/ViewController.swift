7a716128d41da3b30165ad268df3161b71a292ad
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-08-18 08:25:11 -0700

    Interacciones

--- interacciones/ViewController.swift
+++ interacciones/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblMyLabel: UILabel!
+    var contador = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTapButton(_ sender: Any) {
+        contador += 1
+        lblMyLabel.text = "Travis Scott Burger \(contador) "
+    }
+    
 }
 
